35d52716673eb53ed16ab97b4db6651b43167b40
Rogelio Delgado <rogelio@ssf.mx>
2023-08-12 12:06:23 -0600

    Import

---
+++ dwmblocks/blocks.def/cpu_temp.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ICONn="   " # icon for normal temperatures
+ICONc="   " # icon for normal temperatures=
+crit=70 # critical temperature
+
+read -r temp </sys/class/thermal/thermal_zone0/temp
+temp="${temp%???}"
+
+if [ "$temp" -lt "$crit" ] ; then
+    printf "$ICONn%s°C" "$temp"
+else
+    printf "$ICONc%s°C" "$temp"
+fi
