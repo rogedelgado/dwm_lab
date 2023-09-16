7ec160aa431d78bbbaabfdca4a73d71a8a086f4f
Rogelio Delgado <rogelio@ssf.mx>
2023-09-16 17:24:32 -0600

    Add dwmblocks master to the repo

---
+++ dwmblocks-master/blocks.def.h
@@ -0,0 +1,14 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+//
+// When using kill to signal, remember to add 34 to the value that you are configuring here!
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "spotify_status.sh",	          15,		10},
+	{"", "network_manager.sh",					30,		 0},
+	{"", "get_pulse_volume.sh",   			 0,	  11},
+	{"", "calendar.sh",       					60,		 0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = "   ";
+static unsigned int delimLen = 5;
