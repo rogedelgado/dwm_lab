7e721d4e1858f92a4570deacee689a10c02f6598
Rogelio Delgado <rogelio@ssf.mx>
2023-11-29 16:54:22 -0600

    Add git check for documents repo

--- dwmblocks-master/blocks.def.h
+++ dwmblocks-master/blocks.def.h
@@ -3,10 +3,11 @@
 // When using kill to signal, remember to add 34 to the value that you are configuring here!
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "spotify_status.sh",	          15,		10},
-	{"", "network_manager.sh",					30,		 0},
-	{"", "get_pulse_volume.sh",   			 0,	  11},
-	{"", "calendar.sh",       					60,		 0},
+	{"", "spotify_status.sh",	          15,          10},
+	{"", "network_manager.sh",			  30,           0},
+	{"", "get_pulse_volume.sh",   		   0,          11},
+	{"", "calendar.sh",       			  60,           0},
+	{"", "docs_git_changes.sh",       	  30,           0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
