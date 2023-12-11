f9697a592c777eef862e4dfc597e75809c96619e
Rogelio Delgado <rogelio@ssf.mx>
2023-12-11 11:22:47 -0600

    Blocks for battery and light

--- dwmblocks-master/blocks.def.h
+++ dwmblocks-master/blocks.def.h
@@ -3,12 +3,14 @@
 // When using kill to signal, remember to add 34 to the value that you are configuring here!
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "spotify_status.sh",	          15,		10},
-	{"", "get_pulse_volume.sh",   			 0,	  11},
+	{"", "spotify_status.sh",	          15,          10},
+	{"", "get_pulse_volume.sh",   		   0,          11},
 	{"", "battery.sh",       					  15,		12},
 	{"", "backlight.sh",     					  15,		13},
-	{"", "network_manager.sh",					30,		 0},
-	{"", "calendar.sh",       					60,		 0},
+	{"", "network_manager.sh",			  30,           0},
+	{"", "calendar.sh",       			  60,           0},
+	{"", "docs_git_check.sh",       	  30,          12},
+	{"", "todo_git_check.sh",       	  30,          13},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
