4de2321a87fb5033207571ac477c522ed7be18e2
Rogelio Delgado <rogelio@ssf.mx>
2024-10-12 11:29:28 -0600

    Add Columps layout patch

--- dwm-6.4/config.def.h
+++ dwm-6.4/config.def.h
@@ -27,13 +27,14 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-  { "gnome-calculator", NULL,       NULL,                                   0,            1,           -1 },
+	/* class              instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",             NULL,       NULL,                                   0,            1,           -1 },
+    { "gnome-calculator", NULL,       NULL,                                   0,            1,           -1 },
 	{ "Firefox",          NULL,       NULL,                                   1 << 8,       0,           -1 },
 	{ "Pavucontrol",      NULL,       NULL,                                   0,            1,           -1 },
 	{ "MEGAsync",         NULL,       NULL,                                   0,            1,           -1 },
 	{ NULL,               NULL,       "meet.google.com is sharing a window.", 0,            1,           -1 },
+	{ NULL,               NULL,       "Peek",                                 0,            1,           -1 },
 };
 
 /* layout(s) */
@@ -47,6 +48,7 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "|||",      col },
 };
 
 /* key definitions */
@@ -83,6 +85,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
